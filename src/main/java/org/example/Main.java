f291ed40463e5e088c2421d9a88bc6527b3bf875
Ryu-Eun <ektl4m@gmail.com>
2024-05-23 19:29:10 +0900

    Init Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main.main");
+        System.out.println("Main.main");
         System.out.println("Hello world!");
         ///123123123
     }
